a1c28380885f6e904b8df40aca238aafb5cfc7f3
ZTKE <460597494@qq.com>
2022-05-09 00:31:20 +0800

    Add unit code file

---
+++ util/message.py
@@ -0,0 +1,27 @@
+# coding:utf-8
+# author:ila
+normal_code = "normal"
+redirect_code = "skip"
+temporary_redirect_code = "A temporary jump"
+
+empty_param_code = "Request parameters are empty or incorrect"
+validate_param_code = "Request parameters are not normal"
+is_not_json_code = "The data format is not JSON"
+
+other_code = "other error"
+crud_error_code = "Database operation failed"
+header_error_code = "The head error"
+captcha_error_code = "Verification code error"
+id_exist_code = "The ID or record already exists"
+id_notexist_code = "The ID or record does not exist"
+file_notexist_code = "The uploaded file does not exist"
+username_error_code = "User name error"
+password_error_code = "wrong password"
+
+token_error_code = "token error"
+token_expired_code = "token error"
+non_authorized_code = "can not access"
+
+system_error_code = "System level error"
+request_expired_code = "Request expired"
+repeated_request_code = "Repeated requests"
